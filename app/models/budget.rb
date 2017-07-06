1381e9e257445343c0bb9072537fc16625830795
Hector Feurtado <email": null>
2017-07-06 19:55:51 -0400

    fix seed

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,6 +2,6 @@ class Budget < ApplicationRecord
   belongs_to :user
   has_many :goals
 
-  validates :budget_name, :monthly_spend, :goal, presence:true
+  validates :budget_name, :monthly_spend, presence:true
   validates :monthly_spend, numericality: true
 end
