d520dac474bb736557d0696f2469a4e00cc4d063
Hector Feurtado <email": null>
2017-07-07 18:48:04 -0400

    fix migration

--- db/migrate/20170703143621_create_bills.rb
+++ db/migrate/20170703143621_create_bills.rb
@@ -1,13 +1,13 @@
+class CreateBills < ActiveRecord::Migration[5.1]
   def change
-    class CreateBills < ActiveRecord::Migration[5.1]
-    create_table :bills do |t|
-      t.string :bill_name, null:false
-      t.integer :amount, null:false
-      t.integer :due_date, null:false
-      t.string :status, null:false
-      t.integer :user_id, foreign_key:true, null:false
+      create_table :bills do |t|
+        t.string :bill_name, null:false
+        t.integer :amount, null:false
+        t.integer :due_date, null:false
+        t.string :status, null:false
+        t.integer :user_id, foreign_key:true, null:false
 
-      t.timestamps
-    end
+        t.timestamps
+      end
   end
 end
