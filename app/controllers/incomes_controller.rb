ca2ea838bebe3ff3c46f63cd08d6480ff6d93f07
Hector Feurtado <email": null>
2017-07-07 16:49:45 -0400

    income validations, routes

---
+++ app/controllers/incomes_controller.rb
@@ -0,0 +1,36 @@
+class IncomesController < ApplicationController
+  def index
+    incomes = Income.where(user_id:7)
+    render json:incomes
+  end
+
+  def create
+    # binding.pry
+    income = Income.new(
+    source:params["user"]["source"],
+    post_tax_amount: params["user"]["post_tax_amount"],
+    fixed: params["user"]["fixed"],
+    pay_schedule: params["user"]["pay_schedule"],
+    user_id: 7
+    )
+    if income.save
+      render json: income, status: 200
+    else
+      render json: income.errors, status: 422
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+    income = Income.find_by(id:params[:id]).destroy
+    render json: income
+  end
+
+  private
+
+  def income_params
+    params.require(:bill).permit(:source,:post_tax_amount,:fixed,:pay_schedule,:user_id)
+  end
+end
--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,6 @@
 class Income < ApplicationRecord
   belongs_to :user
+
+  validates :source, :post_tax_amount,:pay_schedule, presence:true
+  validates :post_tax_amount, numericality: true
 end
