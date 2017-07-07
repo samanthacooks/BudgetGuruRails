c11a4a98411e06bb6db107d0be518be0938e79b2
Hector Feurtado <email": null>
2017-07-07 15:53:21 -0400

    add goal, delete goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,6 @@
 class Goal < ApplicationRecord
   belongs_to :budget
+
+  validates :goal_name, :amount_saved, :timeframe, :total, :budget_id, presence:true
+  validates :amount_saved, numericality: true
 end
