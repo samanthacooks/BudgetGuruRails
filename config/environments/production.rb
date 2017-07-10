b7bc90bb86643ca486195139d7c51d17cc17e849
Codelikeyouwantit <HectorFeurtado@gmail.com>
2017-07-10 12:13:35 -0400

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,11 +76,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
